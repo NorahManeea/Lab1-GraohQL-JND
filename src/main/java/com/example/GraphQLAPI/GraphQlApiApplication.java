8c5804966acd977a41af6bc9fe878929d0933c15
Norah Almaneea <nouraalmanea0@gmail.com>
2024-03-05 17:49:27 +0300

    first commit

---
+++ src/main/java/com/example/GraphQLAPI/GraphQlApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.GraphQLAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GraphQLAPI/GraphQlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GraphQLAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
